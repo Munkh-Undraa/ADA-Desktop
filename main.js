52577493658d28fc1a678c1ebd832edd5c5e967f
Turmunkh07 <turmunkh.0702@gmail.com>
2023-03-12 13:43:04 +0800

    first-commit

--- main.js
+++ main.js
@@ -2,41 +2,41 @@
 // before starting the app
 
 const electron = require('electron')
-    const { app, BrowserWindow, Menu } = electron
+const { app, BrowserWindow, Menu } = electron
 
-    let mainWindow
+let mainWindow
 
-    const createMainWindow = () => {
-        mainWindow = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false
-            }
-        })
-        mainWindow.loadFile('index.html')
-
-        console.log("Main window created");
-        mainWindow.setMenu(null)
-
-        mainWindow.on('closed', function () {
-            app.quit()
-        })
-    }
-
-    app.whenReady().then(() => {
-        createMainWindow()
-
-        app.on('activate', () => {
-            if (BrowserWindow.getAllWindows().length === 0) {
-                createMainWindow()
-            }
-        })
-    })
-
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'darwin') {
-            app.quit()
+const createMainWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
         }
     })
+    mainWindow.loadFile('index.html')
+
+    console.log("Main window created");
+    mainWindow.setMenu(null)
+
+    mainWindow.on('closed', function () {
+        app.quit()
+    })
+}
+
+app.whenReady().then(() => {
+    createMainWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
